e73af6564f256bec1f2ca995e26a384ed66f7284
tbalogh695@gmail.com <tbalogh695@gmail.com>
2023-04-04 19:52:31 +0200

    added Exception

--- WPFTartalyok/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ WPFTartalyok/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -157,7 +157,7 @@ namespace WPFTartalyok {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPFTartalyok;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPFTartalyok;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
